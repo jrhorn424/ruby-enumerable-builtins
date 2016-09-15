6fa222dda154a5dafaff26dc6370dd80686fcc65
Jeffrey Horn <jeffh@generalassemb.ly>
2016-09-15 11:19:31 -0400

    Add demo for WDI 014

--- bin/range_lab.rb
+++ bin/range_lab.rb
@@ -1,1 +1,12 @@
 # range as enumerable lab
+
+puts "sum all #{(1..10).reduce(:+)}"
+
+puts "sum even #{(1..10).select(&:even?).reduce(:+)}"
+
+puts "sum odd #{(1..10).select(&:odd?).reduce(:+)}"
+
+# HUH?
+ewo = (1..10).each_with_object(Hash.new(0)) do |k, v, o|
+  o[:even] += v if v.even?
+end
--- bin/range_list.rb
+++ bin/range_list.rb
@@ -1,1 +1,31 @@
 # range as enumerable code along
+
+ints = 0..10
+alpha = 'a'..'z'
+hex = 0x0..0xf
+
+ints.each do |n|
+  puts n
+end
+
+puts "maximum int is #{ints.max}"
+
+alpha.each do |a|
+  puts a
+end
+
+puts "maximum alpha is #{alpha.max}"
+
+hex.each do |h|
+  puts h
+end
+
+puts "maximum hex is #{hex.max}"
+
+dunno = alpha.group_by do |a|
+  a.ord % 3 == 0
+end
+
+puts "WAT #{dunno}"
+
+# sentence = 'slow down i am not finished yet'
